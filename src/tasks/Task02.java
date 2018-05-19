a86d1be93dc3eeb409031836e4472390d63c5791
Лойко Евгений <mouvingame@gmail.com>
2018-05-19 14:10:32 +0300

    Update Task02.java

--- src/tasks/Task02.java
+++ src/tasks/Task02.java
@@ -13,29 +13,62 @@ public class Task02 {
             int number2 = scanner.nextInt();
             System.out.println(add(number1, number2));
             System.out.println(subtract(number1, number2));
-            System.out.println(lcm(number1, number2));
-            System.out.println(gcd(number1, number2));
+//             System.out.println(lcm(number1, number2));
+//             System.out.println(gcd(number1, number2));
+            
+//          я так понимаю отрицательные не нунжно обрабатывать потому что там нок и нод еще для отрицательных можно искать))
+//          я забил на отрицательные
+            System.out.println(getNok(number1, number2));
+            System.out.println(getNod(number1, number2));
         } catch (InputMismatchException e) {
             System.out.println("Wrong number");
         }
 
         scanner.close();
     }
-
-    public static int gcd(int num1, int num2) {
-        while (num1 != 0 && num2 != 0) {
-            if (num1 > num2) {
-                num1 = num1 % num2;
+    
+    public static int getNok(int num1, int num2){
+        int bigger = (num1 > num2) ? num1 : num2;
+        
+        int nok = bigger;
+        while (nok >= bigger) {
+            if (nok % num1 == 0 && nok % num2 == 0) {
+                return nok;
             } else {
-                num2 = num2 % num1;
+                nok++;
             }
         }
-        return num1 + num2;
+        return -1;
+    }
+    
+    public static int getNod(int num1, int num2){
+        int smaller = (num1 < num2) ? num1 : num2;
+        
+        int nod = smaller;
+        while (nod <= smaller) {
+            if (num1 % nod == 0 && num2 % nod == 0) {
+                return nod;
+            } else {
+                nod--;
+            }
+        }
+        return -1;
     }
 
-    public static int lcm(int num1, int num2) {
-        return num1 / gcd(num1, num2) * num2;
-    }
+//     public static int gcd(int num1, int num2) {
+//         while (num1 != 0 && num2 != 0) {
+//             if (num1 > num2) {
+//                 num1 = num1 % num2;
+//             } else {
+//                 num2 = num2 % num1;
+//             }
+//         }
+//         return num1 + num2;
+//     }
+
+//     public static int lcm(int num1, int num2) {
+//         return num1 / gcd(num1, num2) * num2;
+//     }
 
     public static int add(int num1, int num2) {
         return num1 + num2;
